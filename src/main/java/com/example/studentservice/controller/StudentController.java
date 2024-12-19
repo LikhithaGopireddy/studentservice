109f3c95b167aa0698b8fa4130350214ad331799
Likhi-G1 <likhithagopireddy2001@gmail.com>
2024-12-19 14:49:40 +0530

    Latest data

--- src/main/java/com/example/studentservice/controller/StudentController.java
+++ src/main/java/com/example/studentservice/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.example.studentservice.controller;
 
+import com.example.studentservice.dto.StudentDTO;
 import com.example.studentservice.entity.Student;
+import com.example.studentservice.repository.StudentRepository;
 import com.example.studentservice.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +16,9 @@ public class StudentController {
 
     @Autowired
     StudentService studentService;
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @GetMapping("/healthStatus")
     public String getHealthStatus() {
@@ -27,5 +33,18 @@ public class StudentController {
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
     }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id){
+        boolean isDeleted = studentService.deleteStudent(id);
+        if(isDeleted){
+            return ResponseEntity.ok("Student deleted successfully");
+        }
+        else {
+            return ResponseEntity.status(404).body("Student not found");
+        }
+
+    }
+
+
 
 }
